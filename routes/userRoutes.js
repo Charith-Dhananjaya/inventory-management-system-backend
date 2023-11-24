664462001d8a4cec2e6db64e50c6f38aff135747
Charith-Dhananjaya <114970084+Charith-Dhananjaya@users.noreply.github.com>
2023-11-24 11:33:08 +0530

    login user begin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/userController");
+const { registerUser, loginUser } = require("../controllers/userController");
 
 
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
